9bd43b1b918eb626e7a97b519f544db10dbf2db8
boto <wirmius@gmail.com>
2022-01-16 17:03:50 +0100

    added simple evaluation for os/mcs/dcs

---
+++ eval_mgenfail.py
@@ -0,0 +1,34 @@
+import os
+
+from gym_PGFS.valuation.comparison_vs_random import compare_mgenfail_mode, draw_mgen_comparison_figure
+from gym_PGFS.envs.PGFS_trials import gym_PGFS_basic_from_config
+from gym_PGFS.config import load_config
+from sys import argv
+
+assert len(argv) == 6, "Requires size positional arguments:" \
+                       " data_path, agent_state_path, config_path, n_samples, out_image"
+
+# transforms = {
+#     'norm': 'normalized ',
+#     'scale': 'scaled ',
+#     'none': ''
+# }
+
+data_path = argv[1]
+agent_path = argv[2]   # 'data/agent_checkpoints/agent_23000.state'
+config = argv[3]    # "./gym_PGFS/configs/config_server_default.yaml"
+n_samples = int(argv[4])
+out_image = argv[5]
+
+conf = load_config(config)  # "./gym_PGFS/configs/config_server_default.yaml"
+run_conf = conf['run']
+env = gym_PGFS_basic_from_config(data_path, conf)  # './data'
+
+os_score, mcs_score, dcs_score = compare_mgenfail_mode(env, agent_path, run_conf, n_samples=n_samples)
+# transform = transforms[conf['env']['scoring_transform']]
+f = draw_mgen_comparison_figure(os_score,
+                                mcs_score,
+                                dcs_score,
+                                ylabel=conf['env']['scoring']['name'],
+                                xlabel=f"step #{agent_path.split('/')[-1].split('.')[0].split('_')[-1]}")
+f.savefig(out_image)
--- gym_PGFS/valuation/comparison_vs_random.py
+++ gym_PGFS/valuation/comparison_vs_random.py
@@ -10,13 +10,15 @@ from ..config import load_config
 from ..envs.PGFS_trials import PGFS_env, gym_PGFS_basic_from_config
 from ..rl.agents import PGFS_agent
 
+from tqdm import tqdm
+
 
 def record_rewards_from_env(env: PGFS_env, agent: PGFS_agent = None, n_samples=100):
     retarray = np.zeros((env.max_steps+1, n_samples))
     ep_counter = 0
     while ep_counter < n_samples:
         s = env.reset()
-        retarray[0, ep_counter] = env.scoring_fn.score(env.rmodel.current_mol)
+        retarray[0, ep_counter] = env.scoring_fn.present_score(env.scoring_fn.score(env.rmodel.current_mol))
         try:
             for i in range(1, env.max_steps+1):
                 if agent:
@@ -93,6 +95,116 @@ def draw_comparison_figure(random_sample: np.ndarray,
     ax.set_xticklabels(rng)
     #ax.legend()
 
+    return fig
 
+
+from gym_PGFS.scorers.scorer import ScorerModeSelector, MGenFailScorer
+def compare_mgenfail_mode(env: Union[PGFS_env, str, os.PathLike],
+                          agent: Union[PGFS_agent, str, os.PathLike],
+                          run_config: Dict = None,
+                          n_samples=100):
+    if not env or isinstance(env, str) or isinstance(env, os.PathLike):
+        cfg = load_config(env)
+        env = gym_PGFS_basic_from_config('data', cfg)
+    if not run_config:
+        run_config = load_config()['run']
+    if isinstance(agent, str) or issubclass(agent, os.PathLike):
+        agent_fname = agent
+        agent = PGFS_agent(env.action_space,
+                           env.observation_space,
+                           env.rng,
+                           **run_config,
+                           gumbel_tau=run_config['g_tau_start'])
+        # not forgetting to load the checkpoint specified
+        agent.load_checkpoint(agent_fname)
+
+    assert isinstance(env.scoring_fn, MGenFailScorer)
+
+    # run the agent with the os score
+    env.scoring_fn.set_mode(ScorerModeSelector.OS_MODE)
+    os_scores = record_rewards_from_env(env, agent, n_samples)
+
+    # evaluate on the mcs score
+    env.scoring_fn.set_mode(ScorerModeSelector.MCS_MODE)
+    mcs_scores = record_rewards_from_env(env, agent, n_samples)
+
+    # evaluate on the dcs score
+    env.scoring_fn.set_mode(ScorerModeSelector.DCS_MODE)
+    dcs_scores = record_rewards_from_env(env, agent, n_samples)
+
+    env.scoring_fn.set_mode(ScorerModeSelector.OS_MODE)
+    return os_scores, mcs_scores, dcs_scores
+
+
+def compare_over_timesteps(env: Union[PGFS_env, str, os.PathLike],
+                           agents: Union[os.PathLike],
+                           run_config: Dict = None,
+                           n_samples=100):
+    assert os.path.exists(agents) and os.path.isdir(agents)
+
+    # load agent states one by one and evaluate
+    agent_filenames = [fname for fname in os.listdir(agents) if os.path.isfile(fname)]
+
+    return_dict = {}
+
+    for afile in agent_filenames:
+        agent_full_path = os.path.join(agents, afile)
+        agent = PGFS_agent(env.action_space,
+                           env.observation_space,
+                           env.rng,
+                           **run_config,
+                           gumbel_tau=run_config['g_tau_start'])
+        # load the checkpoint
+        agent.load_checkpoint(agent_full_path)
+
+        # get the point of evaluation
+        agent_episode = int(afile.split('.')[0].split('_')[1])
+
+        # conduct an experiment
+        os_score, mcs_score, dcs_score = compare_mgenfail_mode(env, agent, run_config, n_samples)
+
+        return_dict[agent_episode] = {'os': os_score, 'mcs': mcs_score, 'dcs': dcs_score}
+
+    return return_dict
+
+
+def draw_mgen_comparison_figure(os_sample: np.ndarray,
+                                mcs_sample: np.ndarray,
+                                dcs_sample: np.ndarray,
+                                size: Tuple = (10, 7),
+                                ylabel = "",
+                                xlabel = ""):
+    fig = plt.figure(figsize=size)
+    ax = fig.add_subplot()
+    plt.ylabel(f"{ylabel} \n(n_samples = {os_sample.shape[0]})")
+    plt.xlabel(xlabel)
+    c, c_highlight = 'cornflowerblue', 'dimgrey'
+    rng = np.arange(os_sample.shape[1])
+    os_box = ax.boxplot(os_sample, patch_artist=True, positions=rng-0.15, widths=0.2,
+                        boxprops=dict(facecolor=c, color=c),
+                        capprops=dict(color=c_highlight),
+                        whiskerprops=dict(color=c_highlight),
+                        flierprops=dict(color=c, markeredgecolor=c),
+                        medianprops=dict(color=c_highlight))
+    c, c_highlight = 'navy', 'darkblue'
+    mcs_box = ax.boxplot(mcs_sample, patch_artist=True, positions=rng+0.15, widths=0.2,
+                         boxprops=dict(facecolor=c, color=c),
+                         capprops=dict(color=c_highlight),
+                         whiskerprops=dict(color=c_highlight),
+                         flierprops=dict(color=c, markeredgecolor=c),
+                         medianprops=dict(color=c_highlight))
+    c, c_highlight = 'red', 'orange'
+    dcs_box = ax.boxplot(dcs_sample, patch_artist=True, positions=rng+0.15, widths=0.2,
+                         boxprops=dict(facecolor=c, color=c),
+                         capprops=dict(color=c_highlight),
+                         whiskerprops=dict(color=c_highlight),
+                         flierprops=dict(color=c, markeredgecolor=c),
+                         medianprops=dict(color=c_highlight))
+    ax.legend([os_box["boxes"][0], mcs_box["boxes"][0], dcs_box["boxes"][0]],
+              ['optimization score', 'model control score', 'data control score'],
+              loc='upper left')
+
+    ax.xaxis.set_ticks(rng)
+    ax.set_xticklabels(rng)
 
     return fig
